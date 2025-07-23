7d8f42b6adf26e9e3afce8ed38e5071be925b6a8
duofusiondharmik <duofusiondharmik@gmail.com>
2025-07-23 12:54:31 +0530

    aaaa

--- src/Routes/upload.ts
+++ src/Routes/upload.ts
@@ -14,6 +14,7 @@ router.post("", (req: any, res: any) => {
             return res.status(400).json(new apiResponse(400, "Image file is missing", {}, {}));
         }
         let imageUrl = config.BACKEND_URL + `/images/${file.filename}`;
+        console.log("Image URL:", imageUrl);
         return res.status(200).json(new apiResponse(200, responseMessage.addDataSuccess("Image"), imageUrl, {}));
     } catch (error) {
         console.log(error)
@@ -33,7 +34,10 @@ router.delete("/", (req: any, res: any) => {
 
         if (!filename) return res.status(400).json(new apiResponse(400, "Invalid image URL", {}, {}));
 
-        const imagePath = path.join(process.cwd(), "images", filename);
+        const imagePath = path.join(process.cwd(), "uploads", filename);
+        console.log("Image URL:", imageUrl);
+
+        console.log("Image path:", imagePath);
         if (!fs.existsSync(imagePath)) return res.status(404).json(new apiResponse(404, "Image not found", {}, {}));
         fs.unlinkSync(imagePath);
 
