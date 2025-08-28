bef831e67f55654b222ab048f66f7688f498bdd6
duofusiondharmik <duofusiondharmik@gmail.com>
2025-08-28 15:54:27 +0530

    all done

--- src/controllers/banner/index.ts
+++ src/controllers/banner/index.ts
@@ -45,10 +45,10 @@ export const editBanner = async (req, res) => {
 export const getAllBanner = async (req, res) => {
   reqInfo(req);
   try {
-    let { type, search, page, limit, activeFilter } = req.query, options: any = { lean: true }, criteria: any = { isDeleted: false };
+    let { type, search, page, limit, actionFilter } = req.query, options: any = { lean: true }, criteria: any = { isDeleted: false };
     if (type) criteria.type = type;
 
-    if (activeFilter) criteria.active = activeFilter;
+    if (actionFilter) criteria.action = actionFilter;
 
     if (search) {
       criteria.title = { $regex: search, $options: 'si' };
